3eb24537d36f78ba10929dbcb2cd9c04f9c747e2
mjacoby18 <156981625+mjacoby18@users.noreply.github.com>
2025-04-21 21:48:07 -0500

    Update routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,15 +5,14 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('src/pages/Todo.vue') },
-      { path: '/help', component: () => import('src/pages/Help.vue') }
+      { path: 'help', component: () => import('src/pages/Help.vue') }
     ]
   },
-  // Always leave this as last one,
-  // but you can also remove it
+  // Always leave this as last one
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
   }
-]
+];
 
-export default routes
+export default routes;
